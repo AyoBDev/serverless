fd6a1a1fd88fab915e9af4aedfa041af0c4bd22b
AyoBDev <ayobamitunmise99@gmail.com>
2025-12-06 02:13:07 +0100

    DynamoDB

--- handler.js
+++ handler.js
@@ -1,14 +1,31 @@
 'use strict';
 
-module.exports.hello = async (event) => {
+module.exports.createNote = async (event) => {
+  return {
+    statusCode: 201,
+    body: JSON.stringify("New Note created successfully!"),
+  };
+};
+
+module.exports.updateNote = async (event) => {
+  const noteId = event.pathParameters.id;
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!'
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify(`Note with id=${noteId} updated successfully!`),
+  };
+};
+
+module.exports.deleteNote = async (event) => {
+  const noteId = event.pathParameters.id;
+  return {
+    statusCode: 200,
+    body: JSON.stringify(`Note with id=${noteId} deleted successfully!`),
+  };
+};
+
+module.exports.getAllNotes = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify("All Notes have been returned!"),
   };
 };
